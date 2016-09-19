f04ab2610d381942ec2c869a2377cff212582b1f
Brian Doane <brianmdoane@gmail.com>
2016-09-18 20:28:25 -0500

    added asi comma ignore, have jquery install issues, project json to dom

--- app/controllers/HomeCtrl.js
+++ app/controllers/HomeCtrl.js
@@ -1,5 +1,11 @@
 'use strict'
 
-app.controller('HomeCtrl', function($scope, $location) {
-	
+app.controller('HomeCtrl', function($scope, $location, ProjectFact) {
+
+	ProjectFact.getProjects()
+	.then((projectObj) => {
+		console.log("projectObj", projectObj)
+		$scope.projects = projectObj
+		console.log("projects", $scope.projects)
+	})	
 })
